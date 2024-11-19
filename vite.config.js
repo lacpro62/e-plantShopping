6933ca8fb2b4a34d8aefe6680f48fe8880160757
lacpro62 <lacnguyen0507@gmail.com>
2024-11-19 03:57:58 -0500

    Changes

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/coursera-paradise-nursery-shopping-application/",
+  base: "/e-plantShopping",
   plugins: [react()],
 })
